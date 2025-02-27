cc4af44c8a1cd0045cd62dd930cecdbf3624b291
Bill Kirscher <wkirscher@gmail.com>
2025-02-26 18:54:55 -0700

    debug

--- caddy/entrypoint.sh
+++ caddy/entrypoint.sh
@@ -6,4 +6,9 @@ export MQTT_SERVICE=${MQTT_SERVICE:-emqx}
 export MQTT_WS_PORT=${MQTT_WS_PORT:-8083}
 export MQTT_ADMIN_PORT=${MQTT_ADMIN_PORT:-18083}
 
+echo "Running Caddy..."
+echo "MQTT_SERVICE: ${MQTT_SERVICE:-emqx}"
+echo "MQTT_WS_PORT: ${MQTT_WS_PORT:-8083}"
+echo "MQTT_ADMIN_PORT: ${MQTT_ADMIN_PORT:-18083}"
+
 exec caddy run --config Caddyfile --adapter caddyfile 2>&1
